2596dca63a1b82ef94c26500b1b76358df13041f
EnriqueGS88 <enrique.gzs@gmail.com>
2022-08-18 23:49:24 +0200

    finished navbar sample

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' />
+          <Route path='/' element={ <Home/> } />
+          <Route path='/reports' element={ <Reports/> } />
+          <Route path='/products' element={ <Products/> } />
         </Routes>
       </Router>
     </>
